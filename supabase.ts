7406ae97ee20f898630a01c3a0f5567661cf7cb5
misforjor <nxnnxpxss@gmail.com>
2026-09-13 16:05:34 +0700

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://rdcwbbkjrggxcdouqrra.supabase.co/rest/v1/'
+const supabaseUrl = 'https://rdcwbbkjrggxcdouqrra.supabase.co'
+
 const supabaseAnonKey = 'sb_publishable_yeqaof7nH0qOa1dgh40Wvg_Jdcsz4_1'
 
 export const supabase = createClient(
