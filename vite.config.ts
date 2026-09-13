b4b62984851550fa86f399a3ec5af5ed82bf4bb6
misforjor <nxnnxpxss@gmail.com>
2026-09-13 21:41:55 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
   const emitSourcemaps = mode === 'development'
 
   return {
-    base: '/kku-lost-found/',
+    base: '/kkulostfound/',
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
       minify: !emitSourcemaps,
